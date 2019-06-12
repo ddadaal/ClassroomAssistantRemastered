eadc286c5b07f7b55b9d36ac2fdebda14d4917db
Chen Junyu <924342513@qq.com>
2019-06-12 10:12:11 +0800

    work on exercise

--- Teacher/app/src/main/kotlin/nju/classroomassistant/teacher/views/exercise/ExerciseController.kt
+++ Teacher/app/src/main/kotlin/nju/classroomassistant/teacher/views/exercise/ExerciseController.kt
@@ -2,7 +2,12 @@ package nju.classroomassistant.teacher.views.exercise
 
 import nju.classroomassistant.teacher.extensions.PageController
 
+enum class ExerciseType{
+    CHOICE,
+    BLANK
+}
 
 
-
-class ExerciseController: PageController(ExerciseInitView::class)
+class ExerciseController: PageController(ExerciseInitView::class){
+    var type:ExerciseType = ExerciseType.BLANK
+}
